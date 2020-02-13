370ea3c4144ee5d72a9f7d270091c0fbbe8372f5
Sam Bryant <bryantsj@gmail.com>
2020-02-13 01:04:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,7 @@
 def position_taken?(board, index)
   if index == " " || "" || nil
     return false
-  elsif index == "X" || index == "O"
-    return true
   else
-    puts "You've done it now!"
+    true
   end
 end
